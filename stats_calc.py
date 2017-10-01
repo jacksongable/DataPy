960dde71ff9ed9564beadc869cee6b14b688d590
Jackson Gable <jackson.gable@protonmail.com>
2017-09-30 22:27:02 -0400

    Adjusted OneVariableStatsCalc class so that it only runs the necessary calculations on the immutable data set once.

--- stats_calc.py
+++ stats_calc.py
@@ -1,72 +1,111 @@
+#
+# This class performs 1-variable statistics calculations on the data set it is initialized with.
+# Since the data set is immutable, each bound method only runs the necessary calculations once,
+# in order to increase efficiency.
+#
 class OneVariableStatsCalc:
 
-    __data = []
+    _data = []
+    _mean = None
+    _max = None
+    _min = None
+    _med = None
+    _q1 = None
+    _q3 = None
+    _range = None
+    _iqr = None
 
     def __init__(self, data):
         data.sort()
-        self.__data = data
+        self._data = data
 
-    def calc_mean (self, data=None):
+    def mean(self, data=None):
+        if data is None and self._mean is not None:
+            return self._mean
         if data is None:
-            data = self.__data
+            data = self._data
         total = 0
         for x in data:
             total += x
-        return total / len(self.__data)
+        self._mean = total / len(self._data)
+        return self._mean
 
-    def calc_max (self):
-        current_max = self.__data[0]
-        for element in self.__data:
+    def max(self):
+        if self._max is not None:
+            return self._max
+        current_max = self._data[0]
+        for element in self._data:
             if element > current_max:
                 current_max = element
-        return current_max
+        self._max = current_max
+        return self._max
 
-    def calc_min (self):
-        current_min = self.__data[0]
-        for element in self.__data:
+    def min(self):
+        if self._min is not None:
+            return self._min
+        current_min = self._data[0]
+        for element in self._data:
             if element < current_min:
                 current_min = element
-        return current_min
+        self._min = current_min
+        return self._min
 
-    def calc_median(self, data=None):
+    def median(self, data=None):
+        if data is None and self._med is not None:
+            return self._med
         if data is None:
-            data = self.__data
+            data = self._data
         max_index = len(data) - 1
 
         if len(data) % 2 == 1: #list length is odd
             median = data[max_index / 2]
         else:
             medians = (data[max_index // 2], data[max_index // 2 + 1])
-            median = self.calc_mean(medians)
-        return median
+            median = self.mean(medians)
+        self._med = median
+        return self._med
 
-    def calc_q1(self):
-        if len(self.__data) % 2 == 1:
-            stop = (len(self.__data) - 1) / 2
+    def q1(self):
+        if self._q1 is not None:
+            return self._q1
+        if len(self._data) % 2 == 1:
+            stop = (len(self._data) - 1) / 2
         else:
-            stop = len(self.__data) / 2
-        return self.calc_median(list[0 : stop])
+            stop = len(self._data) / 2
+        self._q1 = self.median(list[0 : stop])
+        return self._q1
 
-    def calc_q3(self):
-        if len(self.__data) % 2 == 1:
-            start = len(self.__data) / 2 + 1
+    def q3(self):
+        if self._q3 is not None:
+            return self._q3
+        if len(self._data) % 2 == 1:
+            start = len(self._data) / 2 + 1
         else:
-            start = len(self.__data) / 2
-        return self.calc_median(self.__data[start : len(self.__data)])
+            start = len(self._data) / 2
+        self._q3 = self.median(self._data[start : len(self._data)])
+        return self._q3
 
-    def calc_range(self, data=None):
+    def range(self, data=None):
+        if self._range is not None:
+            return self._range
         if data is None:
-            data = self.__data
-        return list[len(data) - 1] - data[0]
+            data = self._data
+        self._range = list[len(data) - 1] - data[0]
+        return self._range
 
     def calc_iqr (self):
-        return self.calc_q3() - self.calc_q1();
+        if self._iqr is not None:
+            return self._iqr
+        self._iqr = self.q3() - self.q1()
+        return self._iqr
 
 
 
 
 
 
+
+#Eventually you'll be able to do a lot more cool stuff.
 
 print ("Hi there! Enter the data. 'C' to stop.");
 
@@ -83,12 +122,6 @@ while True:
 
 calc = OneVariableStatsCalc(list)
 
-#print("Mean", calc.calc_mean())
-#print("Minimum", calc.calc_min())
-print("Q1", calc.calc_q1())
-print("Median", calc.calc_median())
-print("Q3", calc.calc_q3())
-#print("Maximum", calc.calc_max())
-#print("\n")
-#print("Range", calc.calc_range())
-#0print("Interquartile Range", calc.calc_iqr())
+print("Q1", calc.q1())
+print("Median", calc.median())
+print("Q3", calc.q3())
