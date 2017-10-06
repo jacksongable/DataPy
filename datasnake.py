cd2a0e57863107e2099f2abfa12077f1d0ed9c58
Jackson Gable <jackson.gable@protonmail.com>
2017-10-06 11:17:01 -0400

    Expanded on driver code.

--- datasnake.py
+++ datasnake.py
@@ -1,8 +1,44 @@
 from StatsCalc import OneVarStatsCalc
 
+
+#  Prints each option
+def print_options():
+    print "Enter a number to perform calculation."
+    print "1.\tMean"
+    print "2.\tStandard Deviation"
+    print "3.\tRange"
+    print "4.\tInterquartile Range"
+    print "5.\tMinimum"
+    print "6.\tLower Fence"
+    print "7.\tFirst Quartile"
+    print "8.\tMedian"
+    print "9.\tThird Quartile"
+    print "10.\tUpper Fence"
+    print "11.\tMaximum"
+    print "12.\tOutliers"
+    print "13.\tQuit\n"
+
+
+#  Reads, validates, and returns the user's selection
+def parse_selection():
+    while True:
+        choice = raw_input()
+        valid = False
+        if choice.isalnum():
+            if 0 < int(choice) < 14: #  TODO: This line throws a ValueError if the user's input is invalid.
+                valid = True
+        if not valid:
+            print "Oops! Invalid entry."
+            continue
+        else:
+            return int(choice)
+
+
 print "Enter the data, C to stop. Only integers are supported right now."
 data = []
 
+
+#  Loop that initializes data list
 while True:
     integer = raw_input()
     if integer.isalpha():
@@ -13,19 +49,39 @@ while True:
             continue
     data.append(int(integer))
 
+print "Calculations will be performed on the following data set: ", data
+
 stats = OneVarStatsCalc(data)
 
-print "\nPerfect. Here's some one-variable calculations on this data:\n"
-print "Mean\t", stats.mean()
-print "Standard Deviation:\t", stats.std_dev()
-print "Minimum:\t", stats.min()
-print "First Quartile:\t", stats.q1()
-print "Median:\t", stats.median()
-print "Third Quartile:\t", stats.q3()
-print "Maximum:\t", stats.max()
-print "Range:\t", stats.range()
-print "Interquartile range:\t", stats.iqr()
-print "Upper fence:\t", stats.upper_fence()
-print "Lower fence:\t", stats.lower_fence()
-print "Outliers:\t", stats.outliers()
+print_options()
 
+#  Performs calculation specified by user in parse_selection
+while True:
+    selection = parse_selection()
+
+    if selection == 1:
+        print "Mean\t", stats.mean()
+    elif selection == 2:
+        print "Standard Deviation:\t", stats.std_dev()
+    elif selection == 3:
+        print "Range:\t", stats.range()
+    elif selection == 4:
+        print "Interquartile range:\t", stats.iqr()
+    elif selection == 5:
+        print "Minimum:\t", stats.min()
+    elif selection == 6:
+        print "Lower fence:\t", stats.lower_fence()
+    elif selection == 7:
+        print "First Quartile:\t", stats.q1()
+    elif selection == 8:
+        print "Median:\t", stats.median()
+    elif selection == 9:
+        print "Third Quartile:\t", stats.q3()
+    elif selection == 10:
+        print "Upper fence:\t", stats.upper_fence()
+    elif selection == 11:
+        print "Maximum:\t", stats.max()
+    elif selection == 12:
+        print "Outliers:\t", stats.outliers()
+    elif selection == 13:
+        exit(0)
